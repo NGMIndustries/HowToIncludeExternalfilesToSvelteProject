3dd25d39bf49aafafdebc3ce7ccee0c582b80a1f
JohnWRalph <John.Ralph.Git@gmail.com>
2023-03-08 16:28:09 -0600

    first

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { ethers } = require("ethers");
+const { UserAPI,
+    generateKeyPair,
+    ConnectorNames } = require("@loopring-web/loopring-sdk")
+const axios = require('axios/dist/browser/axios.cjs');
+Web3 = require("web3")
 
 
+
+let address;
 async function connectMetaMask() {
     // connectMetaMaskButton.disabled = true;
     const provider = new ethers.providers.Web3Provider(
@@ -26,9 +33,20 @@ async function connectMetaMask() {
 }
 
 
-// function connectMetaMask() {
-//     console.log("Button clicked!");
-// }
+async function fetchLoopringNFts() {
+    console.log("Button clicked!");
+    address = "0x9552cfce60429863D4A7D8205457EC4AC05857dC"
+    const userAPI = new UserAPI({
+        chainId: 1,
+    });
+    const accountInfo = await axios.get(
+        `https://api3.loopring.io/api/v3/account?owner=${address}`
+    );
+        console.log(accountInfo.data.owner)
+}
 
 var button = document.getElementById("connectMetaMask");
 button.addEventListener("click", connectMetaMask);
+
+var button = document.getElementById("fetchLoopring");
+button.addEventListener("click", fetchLoopringNFts);
