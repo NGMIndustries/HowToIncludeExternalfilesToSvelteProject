bc88ac687792aa2502514a17c48a3dcbb5636357
JohnWRalph <John.Ralph.Git@gmail.com>
2023-03-08 16:05:50 -0600

    first

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-function myFunction() {
-    console.log("Button clicked!");
+const { ethers } = require("ethers");
+
+
+async function connectMetaMask() {
+    // connectMetaMaskButton.disabled = true;
+    const provider = new ethers.providers.Web3Provider(
+        (window).ethereum
+    );
+    const walletAddress = await window.ethereum.request({
+        method: "eth_requestAccounts",
+        params: [
+            {
+                eth_accounts: {}
+            }
+        ]
+    });
+
+
+    const accounts = await provider.send("eth_requestAccounts", []);
+    // console.log(provider.send("eth_requestAccounts", []));
+    const providedAccount = accounts[0];
+    console.log(providedAccount)
+    // ethereumAccount.set(providedAccount);
+    return providedAccount;
+
 }
 
+
+// function connectMetaMask() {
+//     console.log("Button clicked!");
+// }
+
 var button = document.getElementById("connectMetaMask");
-button.addEventListener("click", myFunction);
+button.addEventListener("click", connectMetaMask);
